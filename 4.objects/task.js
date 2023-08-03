ccb339c6a597ae88c552888eeeccd2b3f3e58a4f
Sema test <ganzalovdmitriy@gmail.com>
2023-08-03 15:30:13 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,42 +1,53 @@
+'use strict'; // чтоб не расслабляться
 function Student(name, gender, age) {
-    // Ваш код
     this.name = name;
     this.gender = gender;
     this.age = age;
-}
+};
+
+const student10 = new Student("Автандил", "парняга", 20);
+const student20 = new Student("Полуэкт", "парняга", 23);
+const student30 = new Student("Агрипина", "девушка", 21);
+const student40 = new Student("Октавия", "девушка", 23);
 
 Student.prototype.setSubject = function (subjectName) {
-    //ваш код
     this.subject = subjectName;
-}
+};
 
-// ваш код для остальных методов
-Student.prototype.addMark = function (mark) {
+Student.prototype.addMark = function (mark) { // прототип метода добавления одной оценки в экземпляр student
+    if (this.marks === undefined) { // проверка наличия свойства marks
+        let marks = []; // определение(создание) массива для оценок
+        this.marks = marks;
+        this.marks.push(mark); // добавление первой оценки в пустой только что созданный массив оценок экземпляра student
+    } else {
+        //let marks = [];
+        this.marks.push(mark);// добавление оценки в конец массива оценок экземпляра student
+    };
+};
+
+Student.prototype.addMarks = function (...rest) {  // прототип метода добавления нескольких оценок одновременно в экземпляр student
     if (this.marks === undefined) {
-        this.marks = [mark];
-    }
-    else {
-        this.marks.push(mark);
-    }
-}
+        let marks = [];
+        this.marks = marks;
+        this.marks.push(...rest); // заполнение массива marks новыми элементами
+    } else {
+        this.marks.push(...rest); // дополнение массива marks новыми элементами в конец
+    };
+};
 
-Student.prototype.addMarks = function (...rest) {
-    if (this.marks === undefined) {
-        this.marks = [...rest];
-    }
-    else {
-        this.marks.push(...rest);
-    }
-}
+Student.prototype.getAverage = function () { // получение среднего арифметического массива marks в экземпляре student
+    const average = this.marks.reduce((acc, item, idx, arr) => {
+        acc += item;
+        if (idx === arr.length - 1) {
+            return acc / arr.length;
+        } else return acc;
+    })
 
-Student.prototype.getAverage = function () {
-    let sum = 0;
-    this.marks.forEach((mark) => { sum += parseInt(mark) });
-    return sum / this.marks.length;  //в задаче указано, что значение должно быть определно с точностью до 4 знака, однако в автотесте округления нет. 
+    return average; // вывод(возврат) среднего арифметического по запросу .getAverage
 }
 
 Student.prototype.exclude = function (reason) {
-    delete this.subject;
-    delete this.marks;
+    delete this.subject; // удаление свойства subject из экземпляра student
+    delete this.marks; // удаление свойства marks из экземпляра student
     this.excluded = reason;
-}
+};
