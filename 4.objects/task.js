0a27e63be331c7ec638c8f4a231a93321fceb74d
Sema test <ganzalovdmitriy@gmail.com>
2023-08-03 15:43:24 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,53 +1,70 @@
-'use strict'; // чтоб не расслабляться
+
+
 function Student(name, gender, age) {
+
     this.name = name;
     this.gender = gender;
     this.age = age;
+    this.marks = [];
+
 };
 
-const student10 = new Student("Автандил", "парняга", 20);
-const student20 = new Student("Полуэкт", "парняга", 23);
-const student30 = new Student("Агрипина", "девушка", 21);
-const student40 = new Student("Октавия", "девушка", 23);
 
 Student.prototype.setSubject = function (subjectName) {
+
     this.subject = subjectName;
+
 };
 
-Student.prototype.addMark = function (mark) { // прототип метода добавления одной оценки в экземпляр student
-    if (this.marks === undefined) { // проверка наличия свойства marks
-        let marks = []; // определение(создание) массива для оценок
-        this.marks = marks;
-        this.marks.push(mark); // добавление первой оценки в пустой только что созданный массив оценок экземпляра student
-    } else {
-        //let marks = [];
-        this.marks.push(mark);// добавление оценки в конец массива оценок экземпляра student
+
+Student.prototype.addMarks = function (...marksToAdd) {
+
+    let marks = this.marks;
+
+    if ([...marksToAdd] !== [] && this.marks !== undefined) {
+        marks.push(...marksToAdd);
     };
+
 };
 
-Student.prototype.addMarks = function (...rest) {  // прототип метода добавления нескольких оценок одновременно в экземпляр student
-    if (this.marks === undefined) {
-        let marks = [];
-        this.marks = marks;
-        this.marks.push(...rest); // заполнение массива marks новыми элементами
+
+Student.prototype.getAverage = function () {
+
+    arrayOfMarks = this.marks;
+
+    if (arrayOfMarks === [] || arrayOfMarks === undefined) {
+
+        return 0;
+
     } else {
-        this.marks.push(...rest); // дополнение массива marks новыми элементами в конец
-    };
+        const sum = arrayOfMarks.reduce((a, b) => a + b, 0);
+        const avg = (sum / arrayOfMarks.length) || 0;
+        return avg;
+    }
+
 };
-
-Student.prototype.getAverage = function () { // получение среднего арифметического массива marks в экземпляре student
-    const average = this.marks.reduce((acc, item, idx, arr) => {
-        acc += item;
-        if (idx === arr.length - 1) {
-            return acc / arr.length;
-        } else return acc;
-    })
-
-    return average; // вывод(возврат) среднего арифметического по запросу .getAverage
-}
 
 Student.prototype.exclude = function (reason) {
-    delete this.subject; // удаление свойства subject из экземпляра student
-    delete this.marks; // удаление свойства marks из экземпляра student
+
+    delete this.marks;
+    delete this.subject;
     this.excluded = reason;
+
 };
+
+
+
+let student1 = new Student("Василиса", "женский", 19);
+console.log(student1);
+student1.setSubject("Algebra");
+console.log(student1.setSubject);
+console.log(student1.getAverage()); // 0
+student1.addMarks(5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
