a0b8b96236bc8f7f2e8c0aa29a2fe2a94f4505b3
Sema test <ganzalovdmitriy@gmail.com>
2023-08-03 15:25:15 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,27 +1,38 @@
 function Student(name, gender, age) {
+    // Ваш код
     this.name = name;
     this.gender = gender;
     this.age = age;
-    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
+    //ваш код
     this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marksToAdd) {
-    if ('marks' in this === true) {
-        this.marks.push(...marksToAdd);
+// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    }
+    else {
+        this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...rest) {
+    if (this.marks === undefined) {
+        this.marks = [...rest];
+    }
+    else {
+        this.marks.push(...rest);
     }
 }
 
 Student.prototype.getAverage = function () {
-    if ('marks' in this === false || this.marks.length === 0) {
-        return 0
-    }
-    return this.marks.reduce((acc, item) =>
-        acc += item
-        , 0) / this.marks.length;
+    let sum = 0;
+    this.marks.forEach((mark) => { sum += parseInt(mark) });
+    return sum / this.marks.length;  //в задаче указано, что значение должно быть определно с точностью до 4 знака, однако в автотесте округления нет. 
 }
 
 Student.prototype.exclude = function (reason) {
