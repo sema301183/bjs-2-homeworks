f0e8befb85e2f48aa74d2608ef1f60c6b7322708
Sema test <ganzalovdmitriy@gmail.com>
2023-08-11 14:17:40 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,66 +1,57 @@
-﻿function parseCount(inputOfUser) {
+﻿function parseCount(value) {
+    const number = Number.parseInt(value);
+    if (isNaN(number)) {
+        throw new Error('Невалидное значение');
+    }
+    return number;
+}
 
-    let parsResult = Number.parseInt(inputOfUser, 10); //преобразование аргумента в десятичное число
-    if (Number.isNaN(parsResult)) throw new Error('Невалидное значение'); // если результат преобразования не является числом
-
-    return parsResult; // возврат результата преобразования аргумента
-};
-
-function validateCount(inputOfUser2) {
-
+function validateCount(value) {
     try {
-
-        return parseCount(inputOfUser2) // возврат результата преобразования аргумента
-
+        return parseCount(value);
     }
     catch (err) {
-        return err; // возврат ошибки из функции
+        return err;
     }
+}
 
-};
+// HW2
 
 class Triangle {
-
-
-    constructor(a, b, c) { // ввод и обработка параметров, сторон треугольника
+    constructor(a, b, c) {
         this.a = a;
         this.b = b;
         this.c = c;
 
-        if (((a + b) < c) || ((a + c) < b) || ((b + c) < a))
-
-            throw new Error('Треугольник с такими сторонами не существует'); // условие невозможности создать треугольник
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
     }
 
-    getPerimeter() { return this.a + this.b + this.c } //возврат периметра треугольника
+
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
 
     getArea() {
-        let p = this.getPerimeter() / 2; //полупериметр треугольника
-        let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)); //формула Герона
-
-        return Number((s).toFixed(3)); // возврат площади треугольника, обрезка до третьего знака, преобразование в число
+        const p = 0.5 * this.getPerimeter();
+        return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
     }
 
-};
+}
 
 function getTriangle(a, b, c) {
     try {
-        return new Triangle(a, b, c); //проверка создания объекта Треугольник
+        return new Triangle(a, b, c);
     }
-    catch (err) { // вывод ошибки, в случ неудачной попытки создания объекта Треугольник
+    catch (err) {
         return {
-            getArea() {
-                return "Ошибка! Треугольник не существует";
-            },
             getPerimeter() {
-                return "Ошибка! Треугольник не существует";
+                return 'Ошибка! Треугольник не существует';
+            },
+            getArea() {
+                return 'Ошибка! Треугольник не существует';
             }
         }
     }
-};
-/* ПОЯСНЕНИЕ ПРЕПОДАВАТЕЛЯ: Суть замены объекта <в функции getTriangle> в том, что 
-предоставляется безопасный механизм для работы с объектом, 
-который может выбросить ошибку....Аналогично делению 0/0 возвращает NaN, значение как-бы 
-поломано, но ошибки не произошло. И это главное, что ошибки не произошло...У вас ситуация 
-аналогичная: если не получается создать объект, то необходимо хотя бы предоставить механизм 
-работы с этим значением. */
+}
