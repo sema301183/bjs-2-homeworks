de883e48c0ba6b3d7b5b28d3023e4d30c8965530
Sema test <ganzalovdmitriy@gmail.com>
2023-08-11 16:24:05 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,50 +1,50 @@
-﻿function parseCount (item) {
+﻿function parseCount(item) {
     const value = parseFloat(item);
-    if (isNoN(value)){
-    throw new Error("Невалидное значение")
+    if (isNoN(value)) {
+        throw new Error("Невалидное значение")
     }
     return value;
 }
 
-function validateCount (item) {
+function validateCount(item) {
     try {
-    return parseCount(item);
+        return parseCount(item);
     } catch (error) {
-    return error;
+        return error;
     }
 }
 
 
 class Triangle {
- constructor(a, b, c) {
-    if (a + b < c 
-    || a + c < b
-    || b + c < a) {
-    throw new Error ("Треугольник с такими сторонами не существует")
+    constructor(a, b, c) {
+        if (a + b < c
+            || a + c < b
+            || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует")
+        }
+        this.a = a;
+        this.a = b;
+        this.a = c;
     }
-    this.a = a;
-    this.a = b;
-    this.a = c;
-    }
-    
-     getPerimeter() { 
+
+    getPerimeter() {
         return this.a + this.b + this.c;
-     }
-
-     getArea() {
-        const p = this.perimeter / 2;
-     return +Math.sqrt(x: p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(fractionDigits: 3);
-     }
     }
 
+    getArea() {
+        const p = this.perimeter / 2;
+        return +Math.sqrt(x: p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(fractionDigits: 3);
+    }
+}
 
-    function getTriangle(a,b,c) {
-       try {
+
+function getTriangle(a, b, c) {
+    try {
         return new Triangle(a, b, c);
     } catch (error) {
         return {
             getArea() {
-            return "Ошибка! Треугольник не существует";
+                return "Ошибка! Треугольник не существует";
             },
             getPerimeter() {
                 return "Ошибка! Треугольник не существует";
