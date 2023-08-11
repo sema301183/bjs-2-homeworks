b29685fadc2e8de900f4d0ec6cf2661600cc6cb1
Sema test <ganzalovdmitriy@gmail.com>
2023-08-11 16:30:55 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -33,7 +33,7 @@ class Triangle {
 
     getArea() {
         const p = this.perimeter / 2;
-        return +Math.sqrt(x: p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(fractionDigits: 3);
+        return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
     }
 }
 
